9292de8c9a81eb5a253d3da90980cde3aad9371f
LoLii19 <cozlowskaya2010@yandex.ru>
2022-11-05 21:41:46 +0300

    worked on independent work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    // let a = 0
+    let b = 1
+    let[a, setA] = useState(0)
+    function onClickHandler1() {
+        setA(++a)
+        if(a>5) {
+            setA (5)
+        }
+    }
+    function onClickHandler2(){
+        setA(0)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <div className='treebuttonsclass'>
+          <h1 className='aclass'>{a}</h1>
+        <h2 className= 'twobuttonsclass'>
+          <button className='incclass' onClick={onClickHandler1}>inc</button>
+          <button className='resetclass' onClick={onClickHandler2}>reset</button>
+        </h2>
+      </div>
+  )
 }
 
 export default App;
